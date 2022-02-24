97e68d790d879c6acac7220c43422f3ee09dfc71
amarrri-69 <98570914+amarrri-69@users.noreply.github.com>
2022-02-24 08:58:06 -0500

    Auto test using robot object

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAuto.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.sun.tools.javac.comp.Todo;
+
+@Autonomous (name = "Auto with robot object")
+public class RobotAuto extends LinearOpMode {
+    Robot robot = new Robot();
+
+    @Override
+    public void runOpMode() throws NullPointerException {
+        robot.hardwareMap(hardwareMap);
+        waitForStart();
+        robot.encodersForward(6, 1);
+        robot.stopDriving();
+        TelemetryUpdate();
+    }
+
+    public void TelemetryUpdate() {
+        telemetry.addData("Status", "Running");
+        telemetry.addData("Front Left Motor Power", robot.motorFrontLeft.getPower());
+        telemetry.addData("Front Right Motor Power", robot.motorFrontRight.getPower());
+        telemetry.addData("Back Left Motor Power", robot.motorBackLeft.getPower());
+        telemetry.addData("Back Right Motor Power", robot.motorBackRight.getPower());
+        telemetry.addData("Servo Power", robot.wheel.getPower());
+        telemetry.addData("Arm Power", robot.arm.getPower());
+        telemetry.update();
+    }
+}
